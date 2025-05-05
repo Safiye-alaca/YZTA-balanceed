a14340d1e4cc0b9930f2de24cbc2bd37862a305b
Safiye Alaca <safyealaca@gmail.com>
2025-05-05 14:20:34 +0300

    Sync changes before pulling from team main

--- app/models/user.py
+++ app/models/user.py
@@ -8,4 +8,4 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     password = Column(String)
     teacher_id = Column(Integer, nullable=True)  # Yeni eklenen alan
-    class_id = Column(Integer,nullable=True)
+    class_id = Column(Integer, nullable=True)  # Öğrencinin ait olduğu sınıfı belirtir
